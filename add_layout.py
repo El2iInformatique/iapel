5196525add086d315dfc92c9932e320c5b771489
El2i <dev.2@el2i.fr>
2025-06-16 14:33:53 +0200

    Ajout, obligation d'être en admin pour lancer le script

--- add_layout.py
+++ add_layout.py
@@ -1,4 +1,23 @@
+import os
+import ctypes
+import sys
 import sqlite3
+
+def is_admin():
+    try:
+        # Pour les systèmes Linux, macOS
+        # getuid() renvoie 0 si l'utilisateur est root
+        return os.getuid() == 0
+    except AttributeError:
+        # Pour Windows
+        # IsUserAnAdmin() est une fonction de l'API Windows
+        # qui vérifie si l'utilisateur actuel est un administrateur.
+        try:
+            return ctypes.windll.shell32.IsUserAnAdmin() != 0
+        except Exception:
+            # En cas d'erreur (par exemple, si ctypes n'est pas disponible ou l'API échoue)
+            return False
+
 
 def requete_sqlite(db_name, query, params=None):
     conn = None
@@ -23,6 +42,17 @@ def requete_sqlite(db_name, query, params=None):
     finally:
         if conn:
             conn.close()
+
+
+
+
+
+
+
+if not is_admin():
+    print("Veuillez lancer se script en administrateur")
+    sys.exit()
+
 
 
 db_file = "./database/database.sqlite"
