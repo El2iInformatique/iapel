dea0caff5a8147d51d4a75ccdcc953d8d9939215
SweetSkylia <146096843+SweetSkylia@users.noreply.github.com>
2026-07-23 17:02:45 +0200

    hotfix refused
    je suis pas douée

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -577,8 +577,7 @@ class ClientController extends Controller
             // --- MAPPING DEVIS ---
             if ($type === 'devis') {
 
-                $docEntry['status'] = $doc['certifie_file'] ? 'certifie' : ($jsonData['refused'] ? 'refused' : '');
-                // $docEntry['status'] = $doc['certifie_file'] ? 'certifie' : (($doc['refused'] ?? false) ? 'refused' : '');
+                $docEntry['status'] = $doc['certifie_file'] ? 'certifie' : (($jsonData['refused'] ?? false) ? 'refused' : '');
                 $traitTs = $doc['pdf_last'] ?? $doc['certifie_last'];
                 $confTs = $doc['certifie_last'];
 
